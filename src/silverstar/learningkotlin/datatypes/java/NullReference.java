7d3f42b61ebfe79d20a04e86870df1c15ade6b3c
sstocki <sylwester.stocki.rnd@gmail.com>
2021-11-04 23:29:57 +0000

    Section4: DataTypes, Arrays and Null References.

---
+++ src/silverstar/learningkotlin/datatypes/java/NullReference.java
@@ -0,0 +1,9 @@
+package silverstar.learningkotlin.datatypes.java;
+
+public class NullReference {
+
+    public static void main(String[] args) {
+        String str = null;
+        str.toLowerCase();
+    }
+}
