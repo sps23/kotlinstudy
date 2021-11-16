c5261e45ba0ce2e5b53fdd0ee454df0f56578e3c
sstocki <sylwester.stocki.rnd@gmail.com>
2021-11-16 23:17:09 +0000

    Section7: Sequences.

--- src/silverstar/learningkotlin/lambdas/MoreCollectionFunctions.kt
+++ src/silverstar/learningkotlin/lambdas/MoreCollectionFunctions.kt
@@ -8,7 +8,7 @@ fun main() {
 
     val immutableMap =
         mapOf<Int, Car>(
-            1 to Car("blue", "Toyot", 2015),
+            1 to Car("blue", "Toyota", 2015),
             2 to Car("red", "Ford", 2016),
             3 to Car("black", "Honda", 2011),
             4 to Car("black", "Ford", 2016)
