2e662e3b47f02f6116a8b05f8f6e6b514cb09604
sstocki <sylwester.stocki.rnd@gmail.com>
2021-11-27 21:08:19 +0000

    Project refactoring.

--- src/main/silverstar/udemy/learningkotlin/javainterop/challenge1/Challenge9.kt
+++ src/main/silverstar/udemy/learningkotlin/javainterop/challenge1/Challenge9.kt
@@ -2,7 +2,6 @@ package academy.learnprogramming.challenge9
 
 import silverstar.udemy.learningkotlin.javainterop.java.Employee
 
-
 fun main() {
 
     val employee = Employee("Jane", "Smith", 2000)
@@ -10,4 +9,5 @@ fun main() {
     employee.lastName = "Jones"
     employee.salaryLast3Years = floatArrayOf(50000.25f, 54000.60f, 56800.42f)
 
+    println(employee)
 }
