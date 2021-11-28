49797fa51a49de221db3136be4338d289af2260c
sstocki <sylwester.stocki.rnd@gmail.com>
2021-11-28 16:42:07 +0000

    Coursera Week3 - Assignment 'Nice String'- improved solution.

--- src/main/kotlin/silverstar/coursera/kotlin4jd/week3/nicestring/NiceString.kt
+++ src/main/kotlin/silverstar/coursera/kotlin4jd/week3/nicestring/NiceString.kt
@@ -1,16 +1,9 @@
 package silverstar.coursera.kotlin4jd.week3.nicestring
 
 fun String.isNice(): Boolean {
-    val forbiddenSubstrings = setOf("bu", "ba", "be")
-    val hasNoForbiddenSubstrings = forbiddenSubstrings.none { this.contains(it) }
+    val hasNoForbiddenSubstrings = setOf("bu", "ba", "be").none { this.contains(it) }
+    val hasMinNumberOfVowels = this.count { it in setOf('a', 'e', 'i', 'o', 'u') } >= 3
+    val hasDoubleLetter = this.zipWithNext().any { it.first == it.second }
 
-    val vowels = setOf('a', 'e', 'i', 'o', 'u')
-    val minNumberOfVowels = 3
-    val hasMinNumberOfVowels = this.filter { vowels.contains(it) }.length >= minNumberOfVowels
-
-    val containsDoubleLetter = this.zipWithNext().any { it.first == it.second }
-
-    return listOf(hasNoForbiddenSubstrings, hasMinNumberOfVowels, containsDoubleLetter)
-        .filter { it }
-        .size >= 2
+    return listOf(hasNoForbiddenSubstrings, hasMinNumberOfVowels, hasDoubleLetter).count { it } >= 2
 }
