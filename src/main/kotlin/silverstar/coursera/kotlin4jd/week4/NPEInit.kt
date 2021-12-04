0f437db4027cdf1ed4cd5a9b7f14705618bc531f
sstocki <sylwester.stocki.rnd@gmail.com>
2021-12-04 13:04:57 +0000

    Coursera Week4 - NPEInit.

--- src/main/kotlin/silverstar/coursera/kotlin4jd/week4/NPEInit.kt
+++ src/main/kotlin/silverstar/coursera/kotlin4jd/week4/NPEInit.kt
@@ -2,12 +2,12 @@ package silverstar.coursera.kotlin4jd.week4
 
 open class AA(open val value: String) {
     init {
-        println(value.uppercase())
+        println(value.uppercase()) // do not call open attributes in parent
     }
 }
 
 class BB(override val value: String) : AA(value)
 
 fun main() {
-    BB("a")
+    BB("a") // NPE will be thrown here, 'value' is not yet initialized in BB
 }
